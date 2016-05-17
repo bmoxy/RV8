2e7be2ec18030040faa9de1ed8cc73c97d100935
bmoxy <benmoxley@gmail.com>
2016-05-16 22:28:36 -0400

    Add files via upload

--- js/override2.js
+++ js/override2.js
@@ -1,6 +1,5 @@
 var vid = document.getElementById("bgvid");
 var pauseButton = document.querySelector("#bgpause button");
-var menuitemsli = document.querySelector(".navbar-inverse .navbar-nav>li");
 
 function vidFade() {
   vid.classList.add("stopfade");
@@ -14,7 +13,7 @@ vid.pause();
 vidFade();
 }); 
 
-
+// Video Pause Button//
 pauseButton.addEventListener("click", function() {
   vid.classList.toggle("stopfade");
   if (vid.paused) {
@@ -26,11 +25,22 @@ pauseButton.addEventListener("click", function() {
   }
 });
 
-$(document).ready(function(){
+// Nav Bar Click Selector//
+$(function(){
 	$('.navbar-inverse .navbar-nav>li').on('click',function(){
 		$('.navbar-inverse .navbar-nav>li').removeClass('active');
 			$(this).addClass('active');
 			})
 });
 
-
+//Google Maps//
+$(function(){
+function initialize (){
+  var mapProp = {center:new google.maps.LatLng(37.5245782, -77.6333459), 
+    zoom: 10, 
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+});
