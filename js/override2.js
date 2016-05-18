5e75091e8f9f5c95ddcc0235b88ef7bbe8919d45
bmoxy <benmoxley@gmail.com>
2016-05-18 07:56:04 -0400

    Add files via upload

--- js/override2.js
+++ js/override2.js
@@ -37,10 +37,25 @@ $(function(){
 $(function(){
 function initialize (){
   var mapProp = {center:new google.maps.LatLng(37.5245782, -77.6333459), 
+
     zoom: 10, 
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   var map=new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  
+  var UR=new google.maps.LatLng(7.5739607,-77.5418024);
+  var Bryan=new google.maps.LatLng(37.5961435,-77.4742985);
+
+  var marker=new google.maps.Marker({
+    position:UR,
+    });
+  var marker2=new google.maps.Marker({
+    position:Bryan,
+    });
+
+  marker.setMap(map);
+  marker2.setMap(map);
+
 }
 google.maps.event.addDomListener(window, 'load', initialize);
 });
