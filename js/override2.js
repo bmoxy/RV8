25631098b82d9e3ad2b559ee81980cf1c94cb45b
bmoxy <benmoxley@gmail.com>
2016-06-13 11:44:38 -0400

    Add files via upload

--- js/override2.js
+++ js/override2.js
@@ -33,6 +33,11 @@ $(function(){
 			})
 });
 
+$('.nav a').on('click', function(){
+   $('.btn-navbar').click(); //bootstrap 2.x
+   $('.navbar-toggle').click() //bootstrap 3.x by Richard
+});
+ 
 //Google Maps//
 $(function(){
 function initialize (){
